7fac99756eee92667a41663a14db769378e6c80b
Emrah Atalay <emrahatalay92@gmail.com>
2024-08-14 16:52:34 +0300

    Packages Update

---
+++ TCMB.CurrencyService/Exporters/ExcelExporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using TCMB.CurrencyService.Models;
+
+namespace TCMB.CurrencyService.Exporters;
+public class ExcelExporter<T> : IExporter<T>
+{
+    public ExportResult Export(IEnumerable<T> items)
+    {
+        throw new NotImplementedException();
+    }
+}
